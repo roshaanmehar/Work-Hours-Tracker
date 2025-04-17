dd4bd8ce5a17a5b86e46118e5b919e7f2eadf919
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-17 23:52:04 +0100

    file update

---
+++ chronotrack/v2/components/baroque-footer.tsx
@@ -0,0 +1,11 @@
+export default function BaroqueFooter() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <div className="baroque-divider"></div>
+          <p>&copy; {new Date().getFullYear()} Tempus. All rights reserved.</p>
+        </div>
+      </footer>
+    )
+  }
+  
