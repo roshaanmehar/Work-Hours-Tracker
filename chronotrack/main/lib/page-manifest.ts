2ac341075ef4ddfe5de9060419a8cae48bbf8fe6
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-20 02:07:29 +0100

    file update

---
+++ chronotrack/main/lib/page-manifest.ts
@@ -0,0 +1,15 @@
+// This file defines all the pages that should be preloaded
+export const staticPages = [
+    '/',
+    '/history',
+    '/analytics',
+    '/reports',
+    '/expenses',
+    '/admin'
+  ];
+  
+  // This function helps with preloading
+  export function getPreloadablePages() {
+    return staticPages;
+  }
+  
