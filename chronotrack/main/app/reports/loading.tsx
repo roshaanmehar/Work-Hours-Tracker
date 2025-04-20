aab12450d52da63b3872874ede0a89bd7932eb3f
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-20 02:25:03 +0100

    file update

--- chronotrack/main/app/reports/loading.tsx
+++ chronotrack/main/app/reports/loading.tsx
@@ -1,12 +1,3 @@
-import styles from "./loading.module.css"
-
 export default function Loading() {
-  return (
-    <div className={styles.loadingContainer}>
-      <div className={styles.pixelLoader}>
-        <div className={styles.pixelLoaderInner}></div>
-      </div>
-      <p>Loading reports...</p>
-    </div>
-  )
+  return null
 }
