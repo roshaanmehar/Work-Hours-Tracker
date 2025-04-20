ed5d3a39b4ddd5547f129c57b36f14063c3e5a3f
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-20 02:57:52 +0100

    file update

--- chronotrack/main/next.config.mjs
+++ chronotrack/main/next.config.mjs
@@ -56,3 +56,11 @@ const nextConfig = {
 };
 
 export default nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+// Wrap your existing config with withBundleAnalyzer
+module.exports = withBundleAnalyzer({
+  // Your existing config here
+});
