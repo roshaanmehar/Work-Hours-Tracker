0ee7fc5b1d7e3bda36caf9c6515a07a9b698db85
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-20 02:31:12 +0100

    file update

--- chronotrack/main/app/layout.tsx
+++ chronotrack/main/app/layout.tsx
@@ -6,9 +6,6 @@ import Preload from "@/components/preload"
 import PageLoader from "@/components/page-loader"
 import "./globals.css"
 
-// Remove the Press_Start_2P font import and replace with a standard font-family
-// that will be defined in globals.css
-
 export const metadata: Metadata = {
   title: "Pixel Time Tracker",
   description: "Retro pixel art time tracking application",
